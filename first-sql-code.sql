67d64f6aa5b9505ae0c7b3a5c6be072d5adeeb8d
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-13 21:46:01 +0530

    resolv first file codes

--- first-sql-code.sql
+++ first-sql-code.sql
@@ -1,11 +1,11 @@
-create database myFirstsql;
-use myFirstsql;
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    gender ENUM('Male', 'Female', 'Other'),
-    date_of_birth DATE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create database startersql;
+use startersql;
+
+create table users(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+email VARCHAR(100) UNIQUE NOT NULL,
+gender ENUM('Male', 'Female', 'Other'),
+date_of_birth DATE,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-select * from users
