91ea5790c63c0cfaf788895b79485bfaecb21227
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-13 21:28:47 +0530

    my first sql code

---
+++ first-sql-code.sql
@@ -0,0 +1,11 @@
+create database lokendrasql;
+use lokendrasql;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('Male', 'Female', 'Other'),
+    date_of_birth DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+select * from users
