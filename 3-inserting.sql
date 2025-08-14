ab845f89daf4316d3308dddc717bc389df7e2d20
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-14 10:17:11 +0530

    learn about inserting data

---
+++ 3-inserting.sql
@@ -0,0 +1,10 @@
+use startersql; -- jo bhi kaam hoga isme hoga
+
+-- (Table Me Value Dalna) -> INSERT
+INSERT INTO developers (id,name,email,gender,date_of_birth) VALUES 
+(5,'Example Name 4','example5@gmail.com', 'Male', '2005-08-23'),
+(6,'Example Name 5', 'example6@gmail.com','Male', '2005-08-21'),
+(7,'Example Name 6','example7@gmail.com', 'Male', '2005-08-12');
+
+SELECT * FROM developers;
+ 
