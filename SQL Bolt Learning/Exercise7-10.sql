97611ddf20f26f6b749dc768b284962de4318ed9
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-16 14:00:25 +0530

    change the file name to 7-10 and learn about lession 9

--- SQL Bolt Learning/Exercise7-10.sql
+++ SQL Bolt Learning/Exercise7-10.sql
@@ -24,3 +24,21 @@ FROM buildings
   LEFT JOIN employees
     ON building_name = building
 WHERE role IS NULL;
+
+-- SQL Lesson 9: Queries with expressions ==============================
+-- //? List all movies and their combined sales in millions of dollars
+SELECT title , 	(domestic_sales + international_sales) / 1000000 AS combined_sales_inMillion
+FROM movies
+    INNER JOIN boxoffice
+    ON movies.id = movie_id
+
+-- //? List all movies and their ratings in percent
+SELECT title,rating * 10 AS percent
+FROM movies
+    INNER JOIN boxoffice
+    ON movies.id = movie_id
+
+-- //? List all movies that were released on even number years
+SELECT title,year AS even_year
+FROM movies
+WHERE year % 2 = 0
