9ae14a7baabc0dbcc0fd0a7266f3f32535dfa652
Lokendra Nath <nathdeveloper25@gmail.com>
2025-08-16 13:43:52 +0530

    learn about lession 8

--- SQL Bolt Learning/Exercise7.sql
+++ SQL Bolt Learning/Exercise7.sql
@@ -11,3 +11,16 @@ SELECT DISTINCT building_name, role
 FROM buildings 
   LEFT JOIN employees
     ON building_name = building;
+
+-- SQL Lesson 8: A short note on NULLs ==============================
+-- //? Find the name and role of all employees who have not been assigned to a building 
+SELECT name,role 
+FROM employees
+WHERE building IS NULL
+
+-- //? Find the names of the buildings that hold no employees 
+SELECT DISTINCT building_name
+FROM buildings 
+  LEFT JOIN employees
+    ON building_name = building
+WHERE role IS NULL;
